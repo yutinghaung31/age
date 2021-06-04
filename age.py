34bd06a1556112dc2a89f2eee66fb3dc67e78fce
Yu-Ting Huang <yutinghuang31@gmail.com>
2021-06-03 21:39:50 -0700

    upload age.py

---
+++ age.py
@@ -0,0 +1,16 @@
+driving = input('請問你有沒有開過車？')
+if driving != '有' and '沒有':
+	print(只能輸入 有/沒有)
+	raise SystemExit
+age = input('請問你的年齡？')
+age = int(age)
+if driving == '有':
+	if age >= 18:
+	    print('你通過測驗了')
+	else:
+		print('奇怪 你怎麼會有開過車')
+elif driving == '沒有':
+	if age >= 18:
+		print('你可以考駕照了')
+	else:
+		print('你再過幾年就可以考駕照了')
